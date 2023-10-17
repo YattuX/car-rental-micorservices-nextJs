4d58b11e79a22e34447d92d92883e2168a29c095
Yattux <fabid.net@gmail.com>
2023-10-17 19:41:45 +0200

    Create Search with synchronous communucation between services

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,25 +1,43 @@
+using System.Net;
+using Microsoft.VisualBasic;
+using MongoDB.Driver;
+using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
+using SearchService.Data;
+using SearchService.Models;
+using SearchService.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    
+    Console.WriteLine(e);
+}
+
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
